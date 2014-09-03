6fd53d793e040899f7cafdfd469059ad3a6aef77
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-03 10:22:08 -0700

    Update User model, add sass styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :name
+  validates_presence_of :name
 
-  has_many :messages
+  has_many :questions
+  has_many :answers
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "has many questions"
+  pending "has many answers"
+
+  it { should validate_uniqueness_of :name }
+  it { should validate_presence_of :name }
 end

f51558471bf26a6e175254a0e4f0ddc2a83a04bf
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-03 10:26:21 -0700

    Add User & Question association specs

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
 
   validates_presence_of :user_id, :content
 
+  belongs_to :user
   has_many :tags
   has_many :answers
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "has many questions"
   pending "has many answers"
 
   it { should validate_uniqueness_of :name }
   it { should validate_presence_of :name }
+  it { should have_many :questions }
 end

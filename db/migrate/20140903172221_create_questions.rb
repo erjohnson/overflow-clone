1510b248057306874976bc09b0a5ae556912cd28
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-03 10:23:34 -0700

    Update db schema

--- db/migrate/20140903172221_create_questions.rb
+++ db/migrate/20140903172221_create_questions.rb
@@ -1,7 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-
+      t.string :content
+      t.integer :votes
+      t.belongs_to :user
       t.timestamps
     end
   end

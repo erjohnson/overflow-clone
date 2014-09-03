7810ab76b1178c15d2bd6b8a787373f4236964e2
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-03 11:50:57 -0700

    add vote model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903182044) do
+ActiveRecord::Schema.define(version: 20140903184850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20140903182044) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "answer_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

b0794f49152dd6a78fc3dde14dd4f63b6ff8a0a2
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-03 10:47:20 -0700

    Add AJAX new Question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,8 +8,16 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.create!(params[question_params])
+    respond_to do |format|
+      format.html { redirect_to questions_url }
+      format.js
+    end
+  end
+
 private
   def question_params
-    params.require(:question).permit(:content)
+    params.require(:question).permit(:content).merge(user_id: current_user.id)
   end
 end
